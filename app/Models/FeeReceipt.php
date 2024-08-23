0a377be178bd21775eacf6a208b0c7f68247cfd7
kiranshakeel2405 <112563930+kiranshakeel2405@users.noreply.github.com>
2024-08-23 19:09:53 +0500

    ..

--- app/Models/FeeReceipt.php
+++ app/Models/FeeReceipt.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class FeeReceipt extends Model
 {
     use HasFactory;
+    public function students_add()
+    {
+        return $this->belongsTo(Admissionform::class, 'gr_number', 'gr_number');
+    }
 }
